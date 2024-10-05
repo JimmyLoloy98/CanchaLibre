f3643edf37d052f07c8bc7a37a276646abaa0bb5
Jimmy Loloy <loloy.laurencio@gmail.com>
2024-10-05 15:57:23 -0500

    docs: setup hot reloading

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true, // Activa el uso de "polling" si hay problemas en entornos como Docker o WSL
+    },
+    hmr: true, // Habilita el Hot Module Replacement
+  },
 })
